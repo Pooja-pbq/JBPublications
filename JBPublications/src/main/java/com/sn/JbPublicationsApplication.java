55b072d600b3e840c6c97aa00bc2415dac0acbd1
Pooja-pbq <pooja.butla@springernature.com>
2019-11-18 14:49:18 +0530

    initial commit

--- JBPublications/src/main/java/com/sn/JbPublicationsApplication.java
+++ JBPublications/src/main/java/com/sn/JbPublicationsApplication.java
@@ -8,7 +8,7 @@ public class JbPublicationsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JbPublicationsApplication.class, args);
-		System.out.println("Welcome");
+		System.out.println("Pooja");
 	}
 
 }
